0530f6c82fcb4f805cfc8ad7480be5f5351aa978
Jason Woodgate <jason.woodgate@sicon.co.uk>
2020-09-30 15:32:41 +0100

    the customer list is now searchable

--- src/actions/productGroup.js
+++ src/actions/productGroup.js
@@ -15,6 +15,7 @@ export const getProductGroups = () => {
         'http://192.168.122.66/Sicon.Sage200.WebAPI/api/Stock/GetProductGroups',
         // 'http://10.0.0.91/Sicon.Sage200.WebAPI/api/Stock/GetProductGroups',
         // 'http://192.168.1.238/Sicon.Sage200.WebAPI/api/Stock/GetProductGroups',
+      //   'https://cons201.sicon.co.uk:486/Sicon.Sage200.WebAPI/api/Stock/GetProductGroups',
       )
       .then(function (response) {
         dispatch({type: GET_PRODUCT_GROUPS, payload: response.data});
--- src/actions/salesLedger.js
+++ src/actions/salesLedger.js
@@ -1,17 +1,26 @@
 import {GET_CUSTOMER_ACCOUNTS, CUSTOMER_SELECTED} from './types';
 import axios from 'axios';
 
-export const getCustomerAccounts = () => {
+export const getCustomerAccounts = (finishedAction) => {
   return (dispatch, getState) => {
     axios
       .get(
         // 'http://10.0.0.91/Sicon.Sage200.WebAPI/api/Customer/GetCustomers'
+        // 'http://10.0.0.74/Sicon.Sage200.WebAPI/api/Customer/GetCustomers'
         'http://192.168.122.66/Sicon.Sage200.WebAPI/api/Customer/GetCustomers'
         )
       .then(function (response) {
         dispatch({type: GET_CUSTOMER_ACCOUNTS, payload: response.data});
+        
+        if (finishedAction != undefined) {
+          finishedAction(response.data);
+        }
       });
   };
+};
+
+export const customerAccountsRetreived = () => {
+  
 };
 
 export const customerSelected = (customer) => {
